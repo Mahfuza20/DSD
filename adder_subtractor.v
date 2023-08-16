2f135bdf5080b9deb6793ef963cbe0d7b0f2d4ec
Mahfuza Khatun <112787051+Mahfuza20@users.noreply.github.com>
2023-08-17 00:36:53 +0600

    Update adder_subtractor.v
    change code

--- adder_subtractor.v
+++ adder_subtractor.v
@@ -21,18 +21,18 @@ output [3:0]sum;
 output cout;
 
 wire c1, c2, c3;
-/*
+
 wire p, q, r, s;
 
 xor(p, b[0], m);
 xor(q, b[1], m);
 xor(r, b[2], m);
 xor(s, b[3], m);
-*/
-FA fa0(sum[0], c1, a[0], b[0], cin);
-FA fa1(sum[1], c2, a[1], b[1], c1);
-FA fa2(sum[2], c3, a[2], b[2], c2);
-FA fa3(sum[3], cout, a[3], b[3], c3);
+
+  FA fa0(sum[0], c1, a[0], p, cin);
+  FA fa1(sum[1], c2, a[1], q, c1);
+  FA fa2(sum[2], c3, a[2], r, c2);
+  FA fa3(sum[3], cout, a[3], s, c3);
 
 endmodule
 
